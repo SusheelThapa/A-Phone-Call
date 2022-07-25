09f240f73c898fde2a58761a3ae231f5e07b22f8
Susheel Thapa <susheel.thapa@outlook.com>
2022-07-25 22:05:58 +0545

    Server is able to handle the data send by client via file writing.
    When it sees 'CLIENTONECALL' it will print INCOMINGCALL in the file used by Client One and
    when it sees 'CLIENTTWOCALL' it will print INCOMINGCALL in the file used by Client Two.

--- Server/server.cpp
+++ Server/server.cpp
@@ -1,32 +1,82 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
+#include <string>
 
 #define SERVER_FILE "Server/server.txt"
-#define CLIENT_ONE_FILE "../Client/One/client_one.txt"
-#define CLIENT_Two_FILE "../Client/Two/client_two.txt"
+#define CLIENT_ONE_FILE "Client/One/client_one.txt"
+#define CLIENT_TWO_FILE "Client/Two/client_two.txt"
 
 using namespace std;
 
 int main()
 {
+    string message;
+
+    int current_file_size = 0;
+    int previous_file_size = 0;
 
     while (true)
     {
-        system("clear");
 
         /*Opening file for reading*/
         ifstream in_file(SERVER_FILE, ios::in);
 
+        /*Seeking the file pointer to  last*/
         in_file.seekg(0, ios::end);
+
+        /*Return the current position of file pointer*/
         int file_size = in_file.tellg();
 
-        if (file_size > 0)
-        {
-            /*Reading the content of the file*/
+        /*Change the value of file size*/
+        previous_file_size = current_file_size;
+        current_file_size = file_size;
 
-            std::cout << "File has " << file_size << " bytes\n";
+        if (current_file_size != previous_file_size)
+        {
+            /*Clearing the screen*/
+            system("clear");
+
+            /*Reading the content of the file after reseting the file pointer*/
+            in_file.seekg(0, ios::beg);
+            std::getline(in_file, message);
+
+            /*Printing out the message*/
+            std::cout << "Message: " << message << "\n";
+
             /*Processing the content of files*/
+            if (message == "CLIENTONECALL")
+            {
+                fstream client_one_file_pointer;
+                client_one_file_pointer.open(CLIENT_ONE_FILE, ios::out);
+                if (client_one_file_pointer)
+                {
+                    client_one_file_pointer << "INCOMINGCALL";
+
+                    client_one_file_pointer.close();
+                }
+                else
+                {
+                    std::cout << "Error opening the file";
+                }
+            }
+            else if (message == "CLIENTTWOCALL")
+            {
+                fstream client_two_file_pointer;
+
+                client_two_file_pointer.open(CLIENT_TWO_FILE, ios::out);
+
+                if (client_two_file_pointer)
+                {
+                    client_two_file_pointer << "INCOMINGCALL";
+
+                    client_two_file_pointer.close();
+                }
+                else
+                {
+                    std::cout << "Error opening the file";
+                }
+            }
         }
 
         in_file.close();
