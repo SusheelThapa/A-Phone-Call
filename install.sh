42fe15b2bf2afc87b9653a29ebe3b9815b47d913
Susheel Thapa <susheel.thapa@outlook.com>
2022-07-29 19:34:42 +0545

    Script to install all the required libraries for the project

---
+++ install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+sudo apt update && sudo apt upgrade
+
+sudo apt install libsdl2-dev
+sudo apt install libsdl2-image-dev
+sudo apt install libsdl2-ttf-dev
+sudo apt install libsdl2-mixer-dev
+
+sudo apt install g++
+sudo apt install make
