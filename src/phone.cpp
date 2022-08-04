65a2aa676d30847be962671ad7ac9a0226bd812f
Susheel Thapa <susheel.thapa@outlook.com>
2022-08-04 22:33:29 +0545

    Update Phone using the conpet of mulitple inheritance

--- src/phone.cpp
+++ src/phone.cpp
@@ -1,31 +1,11 @@
 #include "phone.hpp"
 
-Phone::Phone(Window &window, std::string name)
+Phone::Phone(Window &window, std::string name) : Screen(window)
 {
-    /*Dynamically allocating the memory for texture of incoming call*/
-    incoming_call = new Texture[5];
-
-    /*Loading the required Texture*/
-    dialpad_screen.loadFromFile(window, "resources/images/dial_pad.png");
-    outgoing_call.loadFromFile(window, "resources/images/outgoing_call.png");
-    incoming_call[0].loadFromFile(window, "resources/images/incoming_call_0.png");
-    incoming_call[1].loadFromFile(window, "resources/images/incoming_call_1.png");
-    incoming_call[2].loadFromFile(window, "resources/images/incoming_call_2.png");
-    incoming_call[3].loadFromFile(window, "resources/images/incoming_call_3.png");
-    incoming_call[4].loadFromFile(window, "resources/images/incoming_call_4.png");
-
-    incoming_call_texture_to_load = 3;
-
-    /*Setting the default screen*/
-    this->current_screen = DIALPAD;
 
     /*Setting the name of the phone*/
     this->name = name;
     this->calling_person_name = " ";
-
-    /*Loading the TTF Font*/
-    big_font = TTF_OpenFont("resources/fonts/freesans.ttf", 56);
-    medium_font = TTF_OpenFont("resources/fonts/freesans.ttf", 32);
 
     /*Setting the value of start time after call connected*/
     start_time = 0;
@@ -33,22 +13,6 @@ Phone::Phone(Window &window, std::string name)
     /*Setting the value of incoming and outgoing call time*/
     incoming_call_time = 0;
     outgoing_call_time = 0;
-
-    /*Loading Dialpad tone*/
-    dialpad_tone[0].setSoundPath("resources/audio/tone-zero.wav");
-    dialpad_tone[1].setSoundPath("resources/audio/tone-one.wav");
-    dialpad_tone[2].setSoundPath("resources/audio/tone-two.wav");
-    dialpad_tone[3].setSoundPath("resources/audio/tone-three.wav");
-    dialpad_tone[4].setSoundPath("resources/audio/tone-four.wav");
-    dialpad_tone[5].setSoundPath("resources/audio/tone-five.wav");
-    dialpad_tone[6].setSoundPath("resources/audio/tone-six.wav");
-    dialpad_tone[7].setSoundPath("resources/audio/tone-seven.wav");
-    dialpad_tone[8].setSoundPath("resources/audio/tone-eight.wav");
-    dialpad_tone[9].setSoundPath("resources/audio/tone-nine.wav");
-
-    /*Loading ringtone*/
-    ringtone.setSoundPath("resources/audio/ringtone.wav");
-    outgoing_tone.setSoundPath("resources/audio/outgoing-tone.wav");
 
     /*Set the phone status*/
     phone_status = SWITCH_OFF;
@@ -70,14 +34,8 @@ Phone::Phone(Window &window, std::string name)
     /*Set the texture to display while switch on the phone*/
     switch_on_texture_to_display = 0;
 
-    /*Load end call tone*/
-    end_call_tone.setSoundPath("resources/audio/end_call_tone.wav");
-
-    /*Load busy_audio*/
-    busy_audio.setSoundPath("resources/audio/busy-tone.wav");
-
     /*Create callended texture*/
-    call_ended.loadFromText(window, medium_font, "Call Ended", {255, 0, 0, 255});
+    call_ended.loadFromText(window, this->getMediumFont(), "Call Ended", {255, 0, 0, 255});
 
     /*Initialize the dial number string*/
     dial_pad_number = " ";
@@ -86,19 +44,9 @@ Phone::Phone(Window &window, std::string name)
     call_connected_audio_recording_status = AudioRecordingStatus::NONE;
 }
 
-void Phone::setScreen(PhoneScreen screen)
-{
-    this->current_screen = screen;
-}
-
 void Phone::setCallingPersonName(std::string name)
 {
     this->calling_person_name = name;
-}
-
-PhoneScreen Phone::getScreen()
-{
-    return current_screen;
 }
 
 void Phone::render(Window &window)
@@ -118,9 +66,10 @@ void Phone::render(Window &window)
         return;
     }
 
-    if (current_screen == DIALPAD)
+    this->renderScreen(window);
+
+    if (this->getCurrentScreen() == DIALPAD)
     {
-        dialpad_screen.render(window, 0, 0, nullptr, nullptr);
 
         SDL_Rect dial_number_render_rect = {
             (window.getWidth() - dial_number.getWidth()) / 2,
@@ -128,27 +77,16 @@ void Phone::render(Window &window)
             dial_number.getWidth(),
             dial_number.getHeight()};
 
-        dial_number.loadFromText(window, big_font, dial_pad_number, {0, 0, 0, 0});
+        dial_number.loadFromText(window, this->getBigFont(), dial_pad_number, {0, 0, 0, 0});
 
         dial_number.render(window, 0, 0, nullptr, &dial_number_render_rect);
     }
-    else if (current_screen == INCOMING_CALL)
+    else if (this->getCurrentScreen() == INCOMING_CALL)
     {
         this->checkIncomingCallTime();
 
-        /*Loading the texture of incoming call*/
-        incoming_call[(incoming_call_texture_to_load / 5)].render(window, 0, 0, nullptr, nullptr);
-
-        /*Choose the texture to render for incoming call*/
-        incoming_call_texture_to_load++;
-
-        if (incoming_call_texture_to_load > 20)
-        {
-            incoming_call_texture_to_load = 0;
-        }
-
         /*Add the name of person who is calling*/
-        calling_person.loadFromText(window, big_font, calling_person_name, {0, 0, 0, 0});
+        calling_person.loadFromText(window, this->getBigFont(), calling_person_name, {0, 0, 0, 0});
 
         SDL_Rect render_calling_person_rect = {
             (window.getWidth() - calling_person.getWidth()) / 2,
@@ -158,14 +96,12 @@ void Phone::render(Window &window)
 
         calling_person.render(window, 0, 0, nullptr, &render_calling_person_rect);
     }
-    else if (current_screen == OUTGOING_CALL)
+    else if (this->getCurrentScreen() == OUTGOING_CALL)
     {
         this->checkOutgoingCallTime();
 
-        outgoing_call.render(window, 0, 0, nullptr, nullptr);
-
         /*Add the person name to whom he/she is calling*/
-        calling_person.loadFromText(window, big_font, calling_person_name, {0, 0, 0, 0});
+        calling_person.loadFromText(window, this->getBigFont(), calling_person_name, {0, 0, 0, 0});
 
         SDL_Rect render_calling_person_rect = {
             (window.getWidth() - calling_person.getWidth()) / 2,
@@ -175,12 +111,11 @@ void Phone::render(Window &window)
 
         calling_person.render(window, 0, 0, nullptr, &render_calling_person_rect);
     }
-    else if (current_screen == CALL_CONNECTED)
+    else if (this->getCurrentScreen() == CALL_CONNECTED)
     {
-        outgoing_call.render(window, 0, 0, nullptr, nullptr);
 
         /*Display the name of the person who is called*/
-        calling_person.loadFromText(window, big_font, calling_person_name, {0, 0, 0, 0});
+        calling_person.loadFromText(window, this->getBigFont(), calling_person_name, {0, 0, 0, 0});
 
         SDL_Rect render_calling_person_rect = {
             (window.getWidth() - calling_person.getWidth()) / 2,
@@ -250,7 +185,7 @@ void Phone::render(Window &window)
 
         call_connected_status.loadFromText(
             window,
-            this->medium_font,
+            this->getMediumFont(),
             message, color);
 
         SDL_Rect call_connected_status_rect = {
@@ -261,13 +196,12 @@ void Phone::render(Window &window)
 
         call_connected_status.render(window, 0, 0, nullptr, &call_connected_status_rect);
     }
-    else if (current_screen == CALL_REJECTED)
+    else if (this->getCurrentScreen() == CALL_REJECTED)
     {
-
-        outgoing_call.render(window, 0, 0, nullptr, nullptr);
+        std::cout << "I am inside call rejected of render";
 
         /*Add the person name to whom he/she is calling*/
-        calling_person.loadFromText(window, big_font, calling_person_name, {0, 0, 0, 0});
+        calling_person.loadFromText(window, this->getBigFont(), calling_person_name, {0, 0, 0, 0});
 
         SDL_Rect render_calling_person_rect = {
             (window.getWidth() - calling_person.getWidth()) / 2,
@@ -277,13 +211,11 @@ void Phone::render(Window &window)
 
         calling_person.render(window, 0, 0, nullptr, &render_calling_person_rect);
     }
-    else if (current_screen == CALL_ENDED)
+    else if (this->getCurrentScreen() == CALL_ENDED)
     {
-        /*Render the outgoing call screen*/
-        outgoing_call.render(window, 0, 0, nullptr, nullptr);
 
         /*Render the name of person*/
-        calling_person.loadFromText(window, big_font, calling_person_name, {0, 0, 0, 0});
+        calling_person.loadFromText(window, this->getBigFont(), calling_person_name, {0, 0, 0, 0});
 
         SDL_Rect render_calling_person_rect = {
             (window.getWidth() - calling_person.getWidth()) / 2,
@@ -334,7 +266,7 @@ void Phone::checkIncomingCallTime()
         this->stopRingtone();
 
         /*Change the current screen to dial pad*/
-        this->current_screen = DIALPAD;
+        this->setCurrentScreen(DIALPAD);
 
         /*Reset incoming call time*/
         incoming_call_time = 0;
@@ -361,35 +293,11 @@ void Phone::checkOutgoingCallTime()
         this->stopOutgoingTone();
 
         /*Change the current screen to dialpad*/
-        this->current_screen = DIALPAD;
+        this->setCurrentScreen(DIALPAD);
 
         /*Reset the outcoming call time*/
         outgoing_call_time = 0;
     }
-}
-
-void Phone::playDialpadTone(int dialpad_number)
-{
-    dialpad_tone[dialpad_number].play();
-}
-
-void Phone::playRingtone()
-{
-    ringtone.play();
-}
-
-void Phone::stopRingtone()
-{
-    ringtone.stop();
-}
-void Phone::playOutgoingTone()
-{
-    outgoing_tone.play();
-}
-
-void Phone::stopOutgoingTone()
-{
-    outgoing_tone.stop();
 }
 
 void Phone::appendDialNumber(std::string num_string)
@@ -411,21 +319,6 @@ void Phone::removeOneDialNumber()
     {
         dial_pad_number.pop_back();
     }
-}
-
-void Phone::playEndCallTone()
-{
-    end_call_tone.play();
-}
-
-void Phone::playBusyTone()
-{
-    busy_audio.play();
-}
-
-void Phone::stopBusyTone()
-{
-    busy_audio.stop();
 }
 
 void Phone::startRecordingAudioMessage()
