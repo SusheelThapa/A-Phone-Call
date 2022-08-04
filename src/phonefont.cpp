9d818b535fcbebc153a9e893731f87e15eb0d11a
Susheel Thapa <susheel.thapa@outlook.com>
2022-08-04 21:40:15 +0545

    Phone font header file to handle the all the text that will be render of Phone class

---
+++ src/phonefont.cpp
@@ -0,0 +1,18 @@
+#include "phonefont.hpp"
+
+PhoneFont::PhoneFont()
+{
+    /*Loading the TTF Font*/
+    big_font = TTF_OpenFont("resources/fonts/freesans.ttf", 56);
+    medium_font = TTF_OpenFont("resources/fonts/freesans.ttf", 32);
+}
+
+TTF_Font *PhoneFont::getBigFont()
+{
+    return big_font;
+};
+
+TTF_Font *PhoneFont::getMediumFont()
+{
+    return medium_font;
+};
