09db9d8046fc4ddd3c256592312383d238daa068
Susheel Thapa <susheel.thapa@outlook.com>
2022-08-04 17:20:19 +0545

    Header file for recording audio

--- include/audio_recording.hpp
+++ include/audio_recording.hpp
@@ -1,16 +1,32 @@
 #pragma once
 
+#include <SDL2/SDL.h>
+
 enum AudioRecordingStatus
 {
     NONE,
     RECORDING,
     RECORDED,
+    PLAYING,
+    PLAYED,
+    SENDING
 };
 
 class AudioRecording
 {
+    Uint32 recording_started_time;
+    Uint32 playing_started_time;
+
 public:
     void startRecordingAudio();
 
-    void stopRecordingAudio();
+    void resetRecordingStartedTime();
+
+    Uint32 getRecordingStartedTime();
+
+    void startPlayingAudio();
+
+    void resetPlayingStartedTime();
+
+    Uint32 getPlayingStartedTime();
 };
--- src/audio_recording.cpp
+++ src/audio_recording.cpp
@@ -5,9 +5,34 @@
 
 void AudioRecording::startRecordingAudio()
 {
-    system("arecord resources/audio/test.wav -c 2 -f S16_LE -r 96000 -d 10 &");
+    this->recording_started_time = SDL_GetTicks();
+
+    system("arecord resources/audio/test.wav -c 2 -f S16_LE -r 96000 -d 5 &");
 }
-void AudioRecording::stopRecordingAudio()
+
+void AudioRecording::resetRecordingStartedTime()
 {
-    /*This one will be written in future*/
+    this->recording_started_time = 0;
+}
+
+Uint32 AudioRecording::getRecordingStartedTime()
+{
+    return this->recording_started_time;
+}
+
+void AudioRecording::startPlayingAudio()
+{
+    this->playing_started_time = SDL_GetTicks();
+
+    system("aplay resources/audio/test.wav &");
+}
+
+void AudioRecording::resetPlayingStartedTime()
+{
+    this->playing_started_time = 0;
+}
+
+Uint32 AudioRecording::getPlayingStartedTime()
+{
+    return this->playing_started_time;
 }
