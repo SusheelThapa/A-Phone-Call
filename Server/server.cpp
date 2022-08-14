b4d667cb627364b1253d407b30fc99eb6ffe45a3
Susheel Thapa <susheel.thapa@outlook.com>
2022-08-15 03:32:35 +0545

    Implement the header file created in Server.cpp file

--- Server/server.cpp
+++ Server/server.cpp
@@ -1,232 +1,81 @@
 #include <iostream>
-#include <fstream>
-#include <stdlib.h>
-#include <string>
 
-#define SERVER_FILE "Server/server.txt"
-#define CLIENT_ONE_FILE "Client/One/client_one.txt"
-#define CLIENT_TWO_FILE "Client/Two/client_two.txt"
-#define LOG_FILE "Server/logs.log"
-
-using namespace std;
+#include <Server/NTC.hpp>
 
 int main()
 {
-    string message;
 
-    int current_file_size = 0;
-    int previous_file_size = 0;
+    NTCServer server;
 
-    /*Opening server file for reading*/
-    ifstream server_file;
-
-    /*File object to write on client file*/
-    fstream client_two_file_pointer;
-    fstream client_one_file_pointer;
-    fstream log_message_file_pointer;
-
-    log_message_file_pointer.open(LOG_FILE, ios::app);
-
-    log_message_file_pointer << "\n";
-    log_message_file_pointer << "Date: " << __DATE__ << "\n";
-    log_message_file_pointer << "Time: " << __TIME__ << "\n";
+    server.updateLogMessage("Date: " + std::string(__DATE__) + "\n");
+    server.updateLogMessage("Date: " + std::string(__TIME__) + "\n");
 
     while (true)
     {
-        /*Open the log file*/
-        log_message_file_pointer.open(LOG_FILE, ios::app);
 
-        /*Open the server file*/
-        server_file.open(SERVER_FILE, ios::in);
-
-        /*Seeking the file pointer to  last*/
-        server_file.seekg(0, ios::end);
-
-        /*Return the current position of file pointer*/
-        int file_size = server_file.tellg();
-
-        /*Change the value of file size*/
-        previous_file_size = current_file_size;
-        current_file_size = file_size;
-
-        if (current_file_size != previous_file_size)
+        if (server.isServerFileChanged())
         {
-            /*Clearing the screen*/
-            // system("clear");
+            server.readMessage();
 
-            /*Reading the content of the file after reseting the file pointer*/
-            server_file.seekg(0, ios::beg);
-            std::getline(server_file, message);
-
-            std::cout << "Message = " << message << "\n";
+            std::cout << "Message = " << server.getMessage() << "\n";
+            std::cout << (server.getMessage() == "CALLCLIENTTWO");
 
             /*Processing the content of files*/
-            if (message == "CALLCLIENTTWO")
+            if (server.getMessage() == "CALLCLIENTTWO")
             {
-                client_two_file_pointer.open(CLIENT_TWO_FILE, ios::out);
-                if (client_two_file_pointer)
-                {
-                    client_two_file_pointer << "CALLFROMCLIENTONE";
-
-                    log_message_file_pointer << "Client One is calling Client Two\n";
-                }
-                else
-                {
-                    std::cout << "Error opening the file";
-                }
+                server.sendMessageToClientTwo("CALLFROMCLIENTONE");
+                server.updateLogMessage("Client One is calling Client Two.\n");
             }
-            else if (message == "CALLCLIENTONE")
+            else if (server.getMessage() == "CALLCLIENTONE")
             {
-
-                client_one_file_pointer.open(CLIENT_ONE_FILE, ios::out);
-
-                if (client_one_file_pointer)
-                {
-                    client_one_file_pointer << "CALLFROMCLIENTTWO";
-
-                    log_message_file_pointer << "Client Two is calling Client One\n";
-                }
-                else
-                {
-                    std::cout << "Error opening the file";
-                }
+                server.sendMessageToClientOne("CALLFROMCLIENTTWO");
+                server.updateLogMessage("Client Two is calling Client One\n");
             }
-            else if (message == "CALLDECLINEDBYCLIENTTWO")
+            else if (server.getMessage() == "CALLDECLINEDBYCLIENTTWO")
             {
-                client_one_file_pointer.open(CLIENT_ONE_FILE, ios::out);
-
-                if (client_one_file_pointer)
-                {
-                    client_one_file_pointer << "CALLDECLINEDFROMCLIENTTWO";
-
-                    log_message_file_pointer << "Client Two has declined the call from Client One\n";
-                }
-                else
-                {
-                    std::cout << "Error opening the file";
-                }
+                server.sendMessageToClientTwo("CALLDECLINEDFROMCLIENTTWO");
+                server.updateLogMessage("Client Two has declined the call from Client One\n");
             }
-            else if (message == "CALLDECLINEDBYCLIENTONE")
+            else if (server.getMessage() == "CALLDECLINEDBYCLIENTONE")
+            {
+                server.sendMessageToClientOne("CALLDECLINEDFROMCLIENTONE");
+                server.updateLogMessage("Client Two has declined the call from Client Two\n");
+            }
+            else if (server.getMessage() == "CALLRECEIVEDBYCLIENTONE")
             {
 
-                client_two_file_pointer.open(CLIENT_TWO_FILE, ios::out);
-
-                if (client_two_file_pointer)
-                {
-                    client_two_file_pointer << "CALLDECLINEDFROMCLIENTONE";
-
-                    log_message_file_pointer << "Client One has declined the call from Client Two\n";
-                }
-                else
-                {
-                    std::cout << "Error opening the file";
-                }
+                server.sendMessageToClientTwo("CALLRECEIVEDFROMCLIENTONE");
+                server.updateLogMessage("Client Two had received the call from Client One\n");
             }
-            else if (message == "CALLRECEIVEDBYCLIENTONE")
+            else if (server.getMessage() == "CALLRECEIVEDBYCLIENTTWO")
             {
-
-                client_two_file_pointer.open(CLIENT_TWO_FILE, ios::out);
-
-                if (client_two_file_pointer)
-                {
-                    client_two_file_pointer << "CALLRECEIVEDFROMCLIENTONE";
-
-                    log_message_file_pointer << "Client Two had received the call from Client One\n";
-                }
-                else
-                {
-                    std::cout << "Error opening the file";
-                }
+                server.sendMessageToClientOne("CALLRECEIVEDFROMCLIENTTWO");
+                server.updateLogMessage("Client Two had received the call from Client Two\n");
             }
-            else if (message == "CALLRECEIVEDBYCLIENTTWO")
+            else if (server.getMessage() == "CALLENDEDBYCLIENTONE")
             {
-
-                client_one_file_pointer.open(CLIENT_ONE_FILE, ios::out);
-
-                if (client_one_file_pointer)
-                {
-                    client_one_file_pointer << "CALLRECEIVEDFROMCLIENTTWO";
-
-                    log_message_file_pointer << "Client One had received the call from Client Two\n";
-                }
-                else
-                {
-                    std::cout << "Error opening the file";
-                }
+                server.sendMessageToClientTwo("CALLENDEDFROMCLIENTONE");
+                server.updateLogMessage("Call has been ended by Client One\n");
             }
-            else if (message == "CALLENDEDBYCLIENTONE")
+            else if (server.getMessage() == "CALLENDEDBYCLIENTTWO")
             {
-                client_two_file_pointer.open(CLIENT_TWO_FILE, ios::out);
-
-                if (client_two_file_pointer)
-                {
-                    client_two_file_pointer << "CALLENDEDFROMCLIENTONE";
-
-                    log_message_file_pointer << "Call has been ended by Client One\n";
-                }
-                else
-                {
-                    std::cout << "Error opening the file";
-                }
+                server.sendMessageToClientOne("CALLENDEDFROMCLIENTTWO");
+                server.updateLogMessage("Call has been ended by Client Two\n");
             }
-            else if (message == "CALLENDEDBYCLIENTTWO")
+
+            else if (server.getMessage() == "AUDIOMESSAGESENDBYCLIENTONE")
             {
-                client_one_file_pointer.open(CLIENT_ONE_FILE, ios::out);
-
-                if (client_one_file_pointer)
-                {
-                    client_one_file_pointer << "CALLENDEDFROMCLIENTTWO";
-
-                    log_message_file_pointer << "Call has been ended by Client Two\n";
-                }
-                else
-                {
-                    std::cout << "Error opening the file";
-                }
+                server.sendMessageToClientTwo("AUDIOMESSAGESENDFROMCLIENTONE");
+                server.updateLogMessage("Audio message has been sent from client one\n");
             }
-
-            else if (message == "AUDIOMESSAGESENDBYCLIENTONE")
+            else if (server.getMessage() == "AUDIOMESSAGESENDBYCLIENTTWO")
             {
-
-                client_two_file_pointer.open(CLIENT_TWO_FILE, ios::out);
-
-                if (client_two_file_pointer)
-                {
-                    client_two_file_pointer << "AUDIOMESSAGESENDFROMCLIENTONE";
-
-                    log_message_file_pointer
-                        << "Audio message has been sent from client one\n";
-                }
-                else
-                {
-                    std::cout << "Error opening the file";
-                }
-            }
-            else if (message == "AUDIOMESSAGESENDBYCLIENTTWO")
-
-            {
-
-                client_one_file_pointer.open(CLIENT_ONE_FILE, ios::out);
-
-                if (client_one_file_pointer)
-                {
-                    client_one_file_pointer << "AUDIOMESSAGESENDFROMCLIENTTWO";
-
-                    log_message_file_pointer
-                        << "Audio message has been sent from client two\n";
-                }
-                else
-                {
-                    std::cout << "Error opening the file";
-                }
+                server.sendMessageToClientOne("AUDIOMESSAGESENDFROMCLIENTTWO");
+                server.updateLogMessage("Audio message has been sent from client two\n");
             }
 
-            client_two_file_pointer.close();
-            client_one_file_pointer.close();
+            server.clearServerFileContent();
         }
-
-        log_message_file_pointer.close();
-        server_file.close();
     }
     return 0;
 }
